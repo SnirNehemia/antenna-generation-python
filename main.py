5cc7c5cc2c73579998d529c8739673a0bd7d79dc
SnirNehemia <snirnehemia@gmail.com>
2023-12-05 16:53:55 +0200

    end of 2023_12_05 - subtraction added
    added subtraction of a few slits.

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ from shapely.geometry import mapping
 import ezdxf.addons.geo
 import ezdxf
 np.random.seed(1)
+
+
 def rectangle(center, size, angle, bounds_polygon, feed_buffer, intersection_bool=0):
     # create a rectangle and returns it. if it is out of bounds it returns 0
     rect = Polygon([(center[0]-size[0]/2, center[1]-size[1]/2),
@@ -41,8 +43,12 @@ def rectangle(center, size, angle, bounds_polygon, feed_buffer, intersection_boo
 
 # define the constant parameters:
 rect_amount = 100
+# add rect properties - center and size
+sub_amount = 10
+sub_size = [[3, 20], [0.5, 1.5]]
 bounds = [(-100, 100), (-100, 100)]
-max_poly_num = 10  # maximum amount of polygons
+max_poly_num = 5  # maximum amount of polygons
+discrete_angle = 45  # discrete angle of rectangles
 
 # define bounding polygon in format of [(x),(y)], for now it's a simple rectangle
 
@@ -55,8 +61,8 @@ bounds_polygon = Polygon([(bounds[0][0], bounds[1][0]),
 feed_length = 5
 buffer_size = feed_length/2
 feed_center = np.random.uniform(-50, 50, (2, 1))
-feed_size = [np.random.uniform(max(feed_length*3,50), 50), 5]
-feed_angle = 0 #np.random.uniform(0, 360)
+feed_size = [np.random.uniform(max(feed_length*3, 50), 50), 5]
+feed_angle = 0  #np.random.uniform(0, 360)
 
 feed_PEC = rectangle(feed_center, feed_size, feed_angle, bounds_polygon, Point([bounds[0][0], bounds[1][0]]))
 feed_poly = rectangle(feed_center, (feed_length, feed_size[1]), feed_angle, bounds_polygon, Point([bounds[0][0], bounds[1][0]]))
@@ -69,18 +75,35 @@ feed_PEC = feed_PEC - feed_poly
 ant_polys = []
 count_failed = 0
 for i in range(rect_amount):
-    center = np.random.uniform(-100, 100, (2, 1))
-    size = [np.random.uniform(10, 50), np.random.uniform(2, 10)]
-    angle = np.random.uniform(0, 360)
+    center = np.round(np.random.uniform(-100, 100, (2, 1)),1)
+    size = np.round([np.random.uniform(10, 50), np.random.uniform(2, 10)],1)
+    angle = np.random.randint(0, int(360/discrete_angle))*discrete_angle
     poly = rectangle(center, size, angle, bounds_polygon, feed_buffer, intersection_bool=1)
     if poly != 0:
         ant_polys.append(poly)
     else:
         count_failed += 1
 print(str(count_failed) + ' rectangles failed')
+ants_merged = unary_union(ant_polys)  # merge the polygons
 
-mergedPolys = unary_union(ant_polys)  # merge the polygons
-ant_polys = list(mergedPolys.geoms)
+# generate sub polygons
+sub_polys = []
+count_failed = 0
+for i in range(sub_amount):
+    center = np.round(np.random.uniform(-100, 100, (2, 1)),1)
+    size = np.round([np.random.uniform(sub_size[0][0], sub_size[0][1]),
+                     np.random.uniform(sub_size[1][0], sub_size[1][1])],1)
+    angle = np.random.randint(0, int(360/discrete_angle))*discrete_angle
+    poly = rectangle(center, size, angle, bounds_polygon, feed_buffer, intersection_bool=1)
+    if poly != 0:
+        sub_polys.append(poly)
+    else:
+        count_failed += 1
+print(str(count_failed) + ' rectangles failed')
+sub_merged = unary_union(sub_polys)  # merge the polygons
+
+ants_merged = unary_union(ants_merged - sub_merged)
+ant_polys = list(ants_merged.geoms)
 ant_polys.sort(key=lambda x: x.area, reverse=True)  # sort from largest to smallest
 
 if len(ant_polys) > max_poly_num:
@@ -88,11 +111,11 @@ if len(ant_polys) > max_poly_num:
 else:
     largest_ant_polys = ant_polys
 
-mergedPolys = unary_union(largest_ant_polys)
+ants_merged = unary_union(largest_ant_polys)
 
 # if i want to count them
-if mergedPolys.geom_type == 'MultiPolygon':
-    polygon_list = list(mergedPolys.geoms)  # extract it back to a polygon list
+if ants_merged.geom_type == 'MultiPolygon':
+    polygon_list = list(ants_merged.geoms)  # extract it back to a polygon list
     polygon_num = len(polygon_list)
 else:
     polygon_num = 1
@@ -100,19 +123,21 @@ else:
 # plot the antenna
 f, ax = plt.subplots()
 gpd.GeoSeries(ant_polys).boundary.plot(ax=ax, alpha=0.2)
-gpd.GeoSeries([mergedPolys]).plot(ax=ax)
+gpd.GeoSeries(ants_merged).plot(ax=ax, alpha=0.5)
 gpd.GeoSeries(feed_PEC).boundary.plot(ax=ax, color='black')
 gpd.GeoSeries(feed_buffer).boundary.plot(ax=ax, color='red', alpha=0.5, linestyle='--')
 gpd.GeoSeries(feed_poly).plot(ax=ax, color='red', alpha=0.5)
+
+gpd.GeoSeries(sub_merged).plot(ax=ax, color='gold', alpha=0.1)
 
 # save the model
 
 os.chdir(r"C:\Users\Snir\OneDrive - Tel-Aviv University\Snir - FemtoNano Group's files\AI RF design\python tests")
 
-if not mergedPolys.geom_type == 'MultiPolygon':
-    PEC_rects = MultiPolygon(mergedPolys)
+if not ants_merged.geom_type == 'MultiPolygon':
+    PEC_rects = MultiPolygon(ants_merged)
 else:
-    PEC_rects = mergedPolys
+    PEC_rects = ants_merged
 PEC_feed = feed_PEC
 feed = feed_poly
 
@@ -130,13 +155,6 @@ for i, file_name in enumerate(files_name_list):
     doc.saveas(file_name + ".dxf")
 
 print('finished')
-
-
-poly = rectangle([-50, -50], [50, 50], 0, bounds_polygon, Point([bounds[0][0], bounds[1][0]]))
-
-p = PEC_rects - poly
-f, ax = plt.subplots()
-gpd.GeoSeries(p).plot(ax=ax, color='black')
 
 
 # # find which rectangles are contained to subtract them(?)
