f652d63453c4a6e7892511fe4dc52eac2ab1e055
SnirNehemia <snirnehemia@gmail.com>
2024-10-21 20:45:51 +0300

    minor change

---
+++ save_env_examples.py
@@ -0,0 +1,41 @@
+import pickle
+
+model_parameters = {
+    'type':3,
+    'plane':'yz-flipped',#changetoyz-flipped
+    #parametersthatchangeboththeantennaandtheenviroment
+    'width':12,#coordinatealongthex(red)axis
+    'height': 75,
+    'length':150,#coordinatealongthev(green)axis
+    'thickness':1,
+    'adx':0.9,
+    'arx':0.9,
+    'ady': 0.9,
+    'ary': 0.85,
+    'adz':0.9,
+    'arz':0.9,
+    'a':0.6,
+    'b':0.8,
+    'c':0.8,
+    'bdx':1,
+    'brx':0.2,
+    'bdy':0.8,
+    'bry':0.75,
+    'bdz':0.8,
+    'brz':0.75,
+    'cdx':1,
+    'crx':0.3,
+    'cdy':0.8,
+    'cry':0.75,
+    'cdz':0.8,
+    'crz':0.75,
+    'ddx':1,
+    'drx':1,
+    'ddy':0.8,
+    'dry':0.75,
+    'ddz':1,
+    'drz':1,
+    'feed_length':2
+}
+
+
